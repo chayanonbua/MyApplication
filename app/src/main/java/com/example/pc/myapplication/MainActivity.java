b6469b1eebbf933baa1edeeaf6b8bbe9bfc02307
chayanonbua <chayanon.bua@gmail.com>
2016-09-06 14:37:56 +0700

    Simple android

--- app/src/main/java/com/example/pc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pc/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.pc.myapplication;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +11,48 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewDebug;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    float num1;
+    EditText textfield1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final TextView text1 = (TextView) findViewById(R.id.textView1);
+        Button but1 = (Button) findViewById(R.id.button1);
+        textfield1 = (EditText)findViewById(R.id.editText);
+        final EditText textfield2 = (EditText)findViewById(R.id.editText2);
+        ImageButton aboutbut = (ImageButton)findViewById(R.id.aboutButton);
+
+
+        but1.setText("OK");
+        but1.setBackgroundColor(Color.GREEN);
+
+       but1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               text1.setText(textfield1.getText().toString()+textfield2.getText().toString());
+           }
+       });
+
+        aboutbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,about2.class));
+            }
+        });
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +85,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
